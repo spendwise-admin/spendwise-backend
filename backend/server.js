463eb850e391ae456fabca2e02103c06cb544d1d
spendwise-admin <spendwise.admin@gmail.com>
2026-03-30 01:55:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,11 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/spendwise";
+  process.env.MONGO_URI;
+if (!MONGO_URI) {
+  console.error("❌ MONGO_URI not found");
+  process.exit(1);
+}
 
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
